cf3696e8c7d344f60a2c389ad584c5e359cbdfd9
Tu Nombre <you@example.com>
2025-11-14 12:13:05 +0100

    introducción galerai

---
+++ galleria.php
@@ -0,0 +1,9 @@
+<?php
+$pageTitle = "Galeria";
+require __DIR__ . "/includes/header.php";
+?>
+
+<h1>Galeria web</h1>
+
+<?php
+require __DIR__ . "/includes/footer.php";
--- includes/header.php
+++ includes/header.php
@@ -16,4 +16,5 @@ if (!isset($pageTitle)) {
         <a href="index.php">Inicio</a>
         <a href="acerca.php">Acerca de</a>
         <a href="cursos.php">Cursos</a>
+        <a href="galleria.php">Galeria</a>
     </nav>
