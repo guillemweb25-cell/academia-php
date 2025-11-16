f18f1b9e41035671694968b1961b00ee8500f5c9
Tu Nombre <you@example.com>
2025-11-16 11:21:35 +0100

    codigo php y estilos

--- galleria.php
+++ galleria.php
@@ -1,9 +1,28 @@
 <?php
 $pageTitle = "Galeria";
 require __DIR__ . "/includes/header.php";
+$dir = __DIR__ . "/uploads";     // carpeta on tens les imatges
+$files = scandir($dir);
+
 ?>
 
 <h1>Galeria web</h1>
-
+<div class="galeria">
+    <?php
+    foreach ($files as $file) {
+        # code...
+        // Ignorar "." i ".."
+        if ($file === "." || $file === "..") {
+            continue;
+        }
+        $cadena=strpos($file, "thumb");
+        if($cadena){
+            ?>
+            <img src="uploads/<?=$file?>" alt="" srcset="">
+            <?php
+        }
+    }
+    ?>
+</div>
 <?php
 require __DIR__ . "/includes/footer.php";
